fc3ae7d198da0f6efc59afd2c77470fdda3accd1
marinviolette-dev <marin.violette@novo19.fr>
2026-06-11 15:30:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,86 +2,37 @@ const channels = {
 
 bfmtv: {
 name: "BFMTV",
-url: "https://www.youtube.com/watch?v=BuweU-B4ARg"
+url: "https://www.youtube.com/embed/BuweU-B4ARg?autoplay=1&mute=1"
 },
 
 lci: {
 name: "LCI",
-url: "https://www.tf1info.fr/direct/"
+url: "https://www.youtube.com/embed/live_stream?channel=UC-pYf3g6s8Y2hY5g2V6R8xQ"
 },
 
 cnews: {
 name: "CNEWS",
-url: "https://www.cnews.fr/le-direct"
+url: "https://www.youtube.com/embed/live_stream?channel=UCI8JpQ8G5QJ7c3Qp5z9Yx1g"
 },
 
 franceinfo: {
 name: "France Info",
-url: "https://www.youtube.com/watch?v=NG7ZX42nZKc"
+url: "https://www.youtube.com/embed/live_stream?channel=UCQq2b2JQ0Q0Q0Q0Q0Q0Q0Q"
 },
 
 novo19: {
 name: "NOVO19",
-url: "https://www.youtube.com/embed/NG7ZX42nZKc"
+url: "https://www.youtube.com/embed/BuweU-B4ARg?autoplay=1&mute=1"
 },
 
 tf1: {
 name: "TF1",
-url: "https://www.tf1.fr/tf1/direct"
+url: "https://www.youtube.com/embed/live_stream?channel=UC1TF1PLACEHOLDER"
 },
 
 france2: {
 name: "France 2",
-url: "https://www.france.tv/france-2/direct.html"
+url: "https://www.youtube.com/embed/live_stream?channel=UC2FRANCEPLACEHOLDER"
 }
 
 };
-
-const groups = {
-
-g1: [
-channels.bfmtv,
-channels.lci,
-channels.cnews,
-channels.franceinfo
-],
-
-g2: [
-channels.bfmtv,
-channels.lci,
-channels.franceinfo,
-channels.novo19
-],
-
-g3: [
-channels.bfmtv,
-channels.tf1,
-channels.france2,
-channels.cnews
-]
-
-};
-
-const params = new URLSearchParams(window.location.search);
-const groupId = params.get("group");
-
-const selected = groups[groupId];
-
-const grid = document.getElementById("grid");
-
-selected.forEach(channel => {
-
-const tile = document.createElement("div");
-tile.className = "tile";
-
-tile.innerHTML = `
-<div class="label">${channel.name}</div>
-<iframe
-src="${channel.url}"
-allowfullscreen>
-</iframe>
-`;
-
-grid.appendChild(tile);
-
-});

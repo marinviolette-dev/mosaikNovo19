4d79c1cd386cb49b1a8bfff7fa81cd7b3b1a2227
marinviolette-dev <marin.violette@novo19.fr>
2026-06-12 14:45:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ url: "https://www.youtube.com/embed/live_stream?channel=UC-pYf3g6s8Y2hY5g2V6R8xQ
 
 cnews: {
 name: "CNEWS",
-url: "https://hls-m015-live-aka-canalplus.akamaized.net/live/disk/cnews-clair-hd/hls-v3-hd-clair/cnews-clair-hd.m3u8", cat:"info", needsProxy:false}"
+url: "https://hls-m015-live-aka-canalplus.akamaized.net/live/disk/cnews-clair-hd/hls-v3-hd-clair/cnews-clair-hd.m3u8"
 },
 
 franceinfo: {
